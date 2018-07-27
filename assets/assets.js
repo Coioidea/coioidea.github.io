8ec62f403069ce0d66341b43b54f8265a4fe2b51
Coioidea <ddllkl@sina.com>
2018-07-27 17:19:20 +0800

    gitalk added and post

--- assets/assets.js
+++ assets/assets.js
@@ -2,8 +2,9 @@
 $(document).ready(function(){
   getHitokoto();
   bgImg();
+  image();
   $("#form").submit(function(getsubmit){
-    $.getJSON("api.json",function(search){
+    $.getJSON("/api.json",function(search){
 
 
     });
@@ -12,15 +13,17 @@ $(document).ready(function(){
 
     function getHitokoto() {
         $.getJSON("https://api.imjad.cn/hitokoto/?charset=utf-8&encode=json", function(data) {
-            $("#hitokoto").html('<p class="bg-success rounded shadow-sm"> >'+data.hitokoto+"</p>");
+            $("#hitokoto").html('<p class="rounded shadow text-white" style="background-color: #272822;"> >'+data.hitokoto+"</p>");
         });
     }
     function bgImg(){
-        $.getJSON("assets/bg.json",function(bg){
+        $.getJSON("/assets/bg.json",function(bg){
           var count = Object.keys(bg).length;
           var num = Math.floor((Math.random()*count)+1);
           var url = bg[num];
           $("body").css("background-image",'url("'+url+'")');
-          console.log(url);
         });
     }
+function image(){
+    $("#post").find("img").addClass("img-fluid");
+}
